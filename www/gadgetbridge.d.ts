2c80bbbfdca6ad84dd3a12d33222d3cca27b7f04
aaru01 <aaru01@16f38ee3-3403-4ab6-acc6-5dc72d0aedef>
2017-09-28 14:36:45 +0000

    added (typescript) typings
    git-svn-id: https://subversion.dfki.de/iuiberlin/projects/MORECARE/dev/hybrid/cordova-plugin-gadgetbridge@2347 16f38ee3-3403-4ab6-acc6-5dc72d0aedef

---
+++ www/gadgetbridge.d.ts
@@ -0,0 +1,202 @@
+
+
+export type ViewType = "ControlCenterv2" | "SettingsActivity" | "MiBandPreferencesActivity" | "AppBlacklistActivity" | "DebugActivity" | "DbManagementActivity" | "DiscoveryActivity" | "MiBandPairingActivity" | "ChartsActivity" | "ConfigureAlarms" | "AlarmDetails";
+
+export interface GadgetbridgePlugin {
+
+  /**
+   * Open a (native) view.
+   *
+   * @param  {String} viewType the name of the view (see this.ACTION_START<n>)
+   * @param  {Function} [successCallback] the success callback (no arguments)
+   * @param  {Function} [errorCallback] the error callback
+   */
+  openView: (viewType: ViewType, successCallback?: SuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+
+  /**
+   * Check, if paired device is (fully) connected.
+   *
+   * @param  {Function} [successCallback] the success callback: successCallback(DeviceInfo | null)
+   * 										where DeviceInfo: {
+   * 											name: String,
+   * 											address: String,
+   * 											model: String,
+   * 											type: String,
+   * 											firmware: String
+   * 										}
+   * @param  {Function} [errorCallback] the error callback
+   */
+  getDeviceInfo: (successCallback: DeviceInfoSuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Check, if paired device is (fully) connected.
+   *
+   * @param  {Function} [successCallback] the success callback: successCallback(boolean)
+   * @param  {Function} [errorCallback] the error callback
+   */
+  isConnected: (successCallback: IsConnectedSuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Connect to paired device.
+   *
+   * @param  {number} [timeout] OPTIONAL
+   *                           timeout for connecting to device (if omitted, default timeout is used).
+   * @param  {Function} [successCallback] the success callback: successCallback()
+   * @param  {Function} [errorCallback] the error callback (e.g. due to timeout)
+   */
+  connect: (timeout?: number | SuccessCallback | ErrorCallback, successCallback?: SuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Add listener for connection-state changes.
+   *
+   * @param  {Function} [successCallback] the success callback: successCallback({address: String, state: String})
+   * @param  {Function} [errorCallback] the error callback
+   */
+  onConnect: (successCallback?: OnConnectionSuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Remove listener for connection-state changes.
+   *
+   * @param  {Function} [successCallback] the success callback: successCallback(didRemove: boolean)
+   * @param  {Function} [errorCallback] the error callback
+   */
+  offConnect: (successCallback?: OffConnectionSuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Get batter level for the paired device.
+   *
+   * @param  {number} [timeout] OPTIONAL
+   *                           timeout for retrieving the batter level (if omitted, default timeout is used).
+   * @param  {Function} [successCallback] the success callback: successCallback(percent)
+   * @param  {Function} [errorCallback] the error callback (e.g. due to timeout)
+   */
+  getBatteryLevel: (timeout?: number | ChargeSuccessCallback | ErrorCallback, successCallback?: ChargeSuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Show notification on tracker/device (immediately).
+   *
+   * @param  {string} message the notification message text TODO title, body (, sender)
+   * @param  {number} [repeat] number of times, for repeating to show text message on device (DEFAULT: 3)
+   * @param  {Function} [successCallback] the success callback: successCallback()
+   * @param  {Function} [errorCallback] the error callback (e.g. if device is not connected)
+   */
+  fireNotification: (message: string, repeat?: number | SuccessCallback | ErrorCallback, successCallback?: SuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Start data synchronization with fitness-tracker device.
+   *
+   * @param  {number} [timeout] OPTIONAL
+   *                           timeout for retrieving the batter level (if omitted, default timeout is used).
+   * @param  {Function} [successCallback] the success callback: successCallback()
+   * @param  {Function} [errorCallback] the error callback (e.g. due to timeout)
+   */
+  synchronize: (timeout?: number | SuccessCallback | ErrorCallback, successCallback?: SuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Get activity data.
+   *
+   * @param  {number} [start] OPTIONAL
+   *                           start timestamp (10 digits) for first data-sample to receive
+   *                           DEFAULT: 0
+   * @param  {number} [end] OPTIONAL
+   *                           end timestamp (10 digits) for last data-sample to receive
+   *                           DEFAULT: now
+   * @param  {Function} [successCallback] the success callback: successCallback(data)
+   * @param  {Function} [errorCallback] the error callback (e.g. no device paired)
+   */
+  retrieveData: (start?: number | SampleDataSuccessCallback | ErrorCallback, end?: number | SampleDataSuccessCallback | ErrorCallback, successCallback?: SampleDataSuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Remove activity data from data base.
+   *
+   * @param  {number} [start] OPTIONAL
+   *                           start timestamp (10 digits) for first data-sample to remove
+   *                           DEFAULT: 0
+   * @param  {number} [end] OPTIONAL
+   *                           end timestamp (10 digits) for last data-sample to remove
+   *                           DEFAULT: now
+   * @param  {Function} [successCallback] the success callback: successCallback()
+   * @param  {Function} [errorCallback] the error callback (e.g. no device paired)
+   */
+  removeData: (start?: number | SuccessCallback | ErrorCallback, end?: number | SuccessCallback | ErrorCallback, successCallback?: SuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Get configuration setting(s).
+   *
+   * @param  {string|Array<string>} [settingsName]
+   * 									the name(s) of the setting(s) to get.
+   * 									If omitted, all settings are returned
+   * @param  {Function} [successCallback] the success callback: successCallback({[id: string]: any})
+   * @param  {Function} [errorCallback] the error callback
+   */
+  getConfig: (settingsName?: string | Array<string> | GetConfigSuccessCallback, successCallback?: GetConfigSuccessCallback | ErrorCallback, errorCallback?: ErrorCallback) => void;
+
+  /**
+   * Set a configuration value.
+   *
+   * @param  {string|{[id:string]: SettingsValue|null} name
+   * 					if string: the name/ID of the setting/configuration field (NOTE: must also supply value argument)
+   * 					if object: the settings (name & value) that should be applied
+   * @param  {SettingsValue|null} [value]
+   * 					the configuration value.
+   * 					Using <code>null</code> will remove the setting, i.e. reset to default value.
+   * @param  {Function} [successCallback] the success callback with the successfully set preference(s) as argument:
+   * 											if arguments name & value: successCallback(string) NOTE: if not successful, the error-callback is invoked
+   * 											if object argument: successCallback(Array<string>)
+   * @param  {Function} [errorCallback] the error callback
+   */
+  setConfig: (name: string | {[id:string]: SettingsValue|null}, value?: SettingsValue|null|SetConfigSuccessCallback, successCallback?: SetConfigSuccessCallback|ErrorCallback, errorCallback?: ErrorCallback) => void;
+}
+
+export type ErrorCallback = (error)=>void;
+export type SuccessCallback = ()=>void;
+export type SampleDataSuccessCallback = (data: Array<DataSample>)=>void;
+export type ChargeSuccessCallback = (chargePercent: number)=>void;
+export type DeviceInfoSuccessCallback = (info: DeviceInfo | null)=>void;
+export type OffConnectionSuccessCallback = (didRemove: boolean)=>void;
+export type OnConnectionSuccessCallback = (connectionState: DeviceConnectionState)=>void;
+export type IsConnectedSuccessCallback = (connected: boolean)=>void;
+export type GetConfigSuccessCallback = (settings: {[id: string]: any})=>void;
+export type SetConfigSuccessCallback = (applied: string | Array<string>)=>void;
+
+export interface DeviceInfo {
+  name: string;
+  address: string;
+  model: string;
+  type: string;
+  firmware: String
+  state: ConnectionState;
+}
+
+export interface DeviceConnectionState {
+  address: string;
+  state: ConnectionState;
+}
+
+export type ConnectionState = "NOT_CONNECTED" |
+  "WAITING_FOR_RECONNECT" |
+  "CONNECTING" |
+  "CONNECTED" |
+  "INITIALIZING" |
+  "AUTHENTICATION_REQUIRED" |
+  "AUTHENTICATING" |
+  "INITIALIZED";
+
+export type SettingsValue = string | number | boolean | Array<string | number | boolean>;
+
+export interface DataSample {
+
+  activity?: number;//double
+  /**light sleep*/
+  sleep1?: number;//float
+  /**deep sleep*/
+  sleep2?: number;//float
+  notWorn?: boolean;
+
+  steps?: number;//integer
+  heartRate?: number;// [0,255]
+
+  raw: number;//integer
+  timestamp: number;//10-digit / UNIX timestamp
+}
